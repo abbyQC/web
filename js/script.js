9de56be8e52d1a84af396be7462987b5d070194e
Chang Qiu <qcabby1217@gmail.com>
2020-02-26 01:15:23 -0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const spans = document.querySelectorAll('h1 span')
+spans.forEach(span => span.addEventListener('mouseover', function(e) {
+    span.classList.add('animated', 'rubberBand')
+}))
+
+spans.forEach(span => span.addEventListener('mouseout', function(e) {
+    span.classList.remove('animated', 'rubberBand')
+}))
+
+
+
+
+const showRequiredCategory = event => {
+    const getId = event.id
+    const links = document.querySelectorAll('.work-category button')
+    for (i = 0; i < links.length; i++) {
+        if (links[i].hasAttribute('class')) {
+            links[i].classList.remove('active')
+        }
+    }
+    event.classList.add('active')
+    const getcategory = document.querySelector(`.category-$(getId)`)
+    const categories = document.querySelectorAll('div[class^="category-"]')
+
+    for (i = 0; i < categories.length; i++) {
+        if (categories[i].hasAttribute('class')) {
+            categories[i].classList.remove('showCategory')
+            categories[i].classList.add('hideCategory')
+        }
+    }
+    getcategory.classList.remove('hideCategory')
+    getcategory.classList.add('showCategory')
+}
