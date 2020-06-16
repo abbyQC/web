5e2b8af8f6e647e8ffa2bc72e25730285d424343
Chang Qiu <qcabby1217@gmail.com>
2020-06-16 16:20:10 -0400

    edit form with bulma

---
+++ static/js/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <ul>
+        <li><a href="#about">about me</a></li>
+        <li><a href="#testimonials">testimonials</a></li>
+        <li><a href="#work">my work</a></li>
+    </ul>
+</template>
+<script>
+    export default {
+
+    }
+</script>
