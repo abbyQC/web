a51b68f3ba76bec5393c8cf8bec1e9ced6f34272
Chang Qiu <qcabby1217@gmail.com>
2020-07-04 18:40:55 -0400

    update info vue component

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,41 +1,49 @@
 <template>
-    <div class="info">
-        <span>H</span>
-        <span class="m-left">i</span>
-        <span class="m-left">,</span>
-        <br>
-        <span>I</span>
-        <span class="m-left">'</span>
-        <span class="m-left">m</span>
-        <span class="name">C</span>
-        <span class="m-left name">h</span>
-        <span class="m-left name">a</span>
-        <span class="m-left name">n</span>
-        <span class="m-left name">g</span>
-        <span class="m-left ">,</span>
+    <div>
 
-        <br>
-        <br>
-        <span>F</span>
-        <span class="m-left">r</span>
-        <span class="m-left">o</span>
-        <span class="m-left">n</span>
-        <span class="m-left">t</span>
-        <span class="m-left">-</span>
-        <span class="m-left">e</span>
-        <span class="m-left">n</span>
-        <span class="m-left">d</span>
-        <br>
-        <span>D</span>
-        <span class="m-left">e</span>
-        <span class="m-left">v</span>
-        <span class="m-left">e</span>
-        <span class="m-left">l</span>
-        <span class="m-left">o</span>
-        <span class="m-left">p</span>
-        <span class="m-left">e</span>
-        <span class="m-left">r</span>
-        <span class="m-left">.</span>
+
+        <div class="info">
+            <span>H</span>
+            <span class="m-left">i</span>
+            <span class="m-left">,</span>
+            <br>
+            <span>I</span>
+            <span class="m-left">'</span>
+            <span class="m-left">m</span>
+            <span class="name">C</span>
+            <span class="m-left name">h</span>
+            <span class="m-left name">a</span>
+            <span class="m-left name">n</span>
+            <span class="m-left name">g</span>
+            <span class="m-left ">,</span>
+
+            <br>
+            <br>
+            <span>F</span>
+            <span class="m-left">r</span>
+            <span class="m-left">o</span>
+            <span class="m-left">n</span>
+            <span class="m-left">t</span>
+            <span class="m-left">-</span>
+            <span class="m-left">e</span>
+            <span class="m-left">n</span>
+            <span class="m-left">d</span>
+            <br>
+            <span>D</span>
+            <span class="m-left">e</span>
+            <span class="m-left">v</span>
+            <span class="m-left">e</span>
+            <span class="m-left">l</span>
+            <span class="m-left">o</span>
+            <span class="m-left">p</span>
+            <span class="m-left">e</span>
+            <span class="m-left">r</span>
+            <span class="m-left">.</span>
+        </div>
+        <div class="jb-mono-regular">
+            <p class="sub" id="t1">web development/ IOS development</p>
+            <p class="sub" id="t2">Java/ MySQL/ Verilog</p>
+        </div>
     </div>
 </template>
 
@@ -52,6 +60,11 @@
         font-size: 3rem;
         cursor: context-menu;
         margin-top: 140px;
+    }
+
+    .jb-mono-regular {
+        font-family: "JetBrains Mono";
+        font-size: 0.8rem;
     }
 
 
